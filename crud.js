87e66dffcac6fdda7c5d4e28d125344c35156c2f
edcruz29 <galvaocruz16@gmail.com>
2021-05-27 20:17:51 -0300

    criação do gitignore

--- crud.js
+++ crud.js
@@ -1,25 +1,19 @@
-const express = require ('express');
+const express = require("express");
 const app = express();
 app.use(express.json());
 
 const port = 3000;
 
 const usuariosEduardo = {
-    id:1,
-    nome:"Eduardo",
-    idade:31
+  id: 1,
+  nome: "Eduardo",
+  idade: 31,
 };
 
-const usuarios = [usuariosEduardo]
+const usuarios = [usuariosEduardo];
 
-
-
-
-
-app.get('/usuarios',(req,res)=>{
-    res.json(usuarios)
+app.get("/usuarios", (req, res) => {
+  res.json(usuarios);
 });
 
-app.listen(port, ()=>
-    console.log(`Server is up and running on port ${port}` )
-);
+app.listen(port, () => console.log(`Server is up and running on port ${port}`));
