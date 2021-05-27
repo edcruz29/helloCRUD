f53aad3156106b33eb28c26592ce3ea697ce7b87
edcruz29 <galvaocruz16@gmail.com>
2021-05-27 20:10:21 -0300

    server up

--- crud.js
+++ crud.js
@@ -1,6 +1,25 @@
 const express = require ('express');
 const app = express();
+app.use(express.json());
 
-app.listen(3000, ()=>
-    console.log("Servidor on")
+const port = 3000;
+
+const usuariosEduardo = {
+    id:1,
+    nome:"Eduardo",
+    idade:31
+};
+
+const usuarios = [usuariosEduardo]
+
+
+
+
+
+app.get('/usuarios',(req,res)=>{
+    res.json(usuarios)
+});
+
+app.listen(port, ()=>
+    console.log(`Server is up and running on port ${port}` )
 );
