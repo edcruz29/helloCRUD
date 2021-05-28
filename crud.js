05a057a2a914bf610c80b2c53cdf2f866d8943ca
edcruz29 <galvaocruz16@gmail.com>
2021-05-27 21:45:56 -0300

    atualizar e delete criado

--- crud.js
+++ crud.js
@@ -17,7 +17,7 @@ app.get("/usuarios", (req, res) => {
 });
 
 app.post('/usuarios', (req,res) =>{
-    const{nome, idade} = req.body;
+    const {nome, idade} = req.body;
     const id = usuarios.length;
 
     const novoUsuario = {
@@ -27,7 +27,28 @@ app.post('/usuarios', (req,res) =>{
     };
 
     usuarios.push(novoUsuario)
-    res.json(novoUsuario);
+    res.status(201).json(novoUsuario).send();
+})
+
+app.put('/usuarios/:id', (req,res)=>{
+    const {id} = req.params
+    const {nome, idade} = req.body;
+
+    const usuario = usuarios.find(usuario => usuario.id === Number(id))
+    usuario.nome = nome;
+    usuario.idade = idade;
+
+    res.json(usuario);
+});
+
+app.delete('/usuarios/:id', (req,res) =>{
+    const{ id } = req.params;
+    const index = usuarios.findIndex(usuario => usuario.id === Number(id));
+    usuarios.splice(index,1)
+
+    res.status(204).send();
+
+
 })
 
 app.listen(port, () =>{
